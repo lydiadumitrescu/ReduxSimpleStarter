5378fd7f9764b47cb04bf4ec698f11f917ae0d16
CODE40\lydia.dumitrescu <lydia.dumitrescu@maxcode.net>
2017-10-10 17:31:50 +0300

    enable sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    sourceMapFilename: '[file].map'
   },
   module: {
     loaders: [{
@@ -22,5 +23,6 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  devtool: 'source-map'
 };
