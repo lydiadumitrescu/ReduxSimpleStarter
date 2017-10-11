af1da6feeb4af967a2ca084a7fccc860e0f4faf0
CODE40\lydia.dumitrescu <lydia.dumitrescu@maxcode.net>
2017-10-11 17:06:47 +0300

    test

---
+++ src/components/posts_new.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+	renderField (field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input
+					className="form-control"
+					type="text"
+					{...field.input}				
+				/>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<form>
+				<Field
+					name="title"
+					label="Title"
+					component={this.renderField}
+				/>
+				<Field
+					name="tags"
+					label="Tags"
+					component={this.renderField}
+				/>
+				<Field
+					name="content"
+					label="Post content"
+					component={this.renderField}
+				/>
+				
+			</form>
+			
+		);
+	}
+}
+
+function validate(values) {
+	const errors = {};
+
+	if(!values.title) {
+		errors.title = "Enter a title!"
+	}
+
+	return errors;
+}
+
+export default reduxForm({
+	form: 'PostsNewForm'
+})(PostsNew);
